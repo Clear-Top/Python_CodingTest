9cb176a3064fe3b8d70c757edf7ec2dbbc5f0bc5
Clear-Top <6tank1004@gmail.com>
2023-05-20 12:13:32 +0900

    아직해결X

--- BaekJoon/Greedy/(1541) 잃어버린괄호.py
+++ BaekJoon/Greedy/(1541) 잃어버린괄호.py
@@ -3,16 +3,16 @@
 strip 함수로 맨앞에 0 들을 지우기
 첫 - 바로 앞부터 마지막까지 중괄호 씌워서 식계산하기 
 '''
-formula_str = input()
-formula_modify = []
-for i in formula_str:
-    if i=='+' or i=='-':
-        list_str = list(formula_str[:formula_str.find(i)])
-        formula_modify.append(list_str)
-    else:
-        formula_modify.append([i])
-        
-print(formula_modify)
-        # formula_modify.append(list)
-# print(formula_modify)
-# print(formula_str[:formula_str.find('-')])
+eqa = input()
+temp = []
+
+# temp = list(map(int, eqa.split("-")))
+temp = eqa.split("-")
+
+result = 0
+for sub_eqa in temp[1:]:
+    result += sum(map(int, sub_eqa.split("+")))
+
+
+print(result)
+
