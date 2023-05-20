ef78dddb283bd6db8f5d8a83f17c07befb827d71
Clear-Top <6tank1004@gmail.com>
2023-05-21 03:44:37 +0900

    해결

--- BaekJoon/Greedy/(1541) 잃어버린괄호.py
+++ BaekJoon/Greedy/(1541) 잃어버린괄호.py
@@ -4,15 +4,13 @@ strip 함수로 맨앞에 0 들을 지우기
 첫 - 바로 앞부터 마지막까지 중괄호 씌워서 식계산하기 
 '''
 eqa = input()
-temp = []
-
-# temp = list(map(int, eqa.split("-")))
 temp = eqa.split("-")
 
 result = 0
-for sub_eqa in temp[1:]:
-    result += sum(map(int, sub_eqa.split("+")))
+result = sum(map(int, temp[0].split('+')))
 
+for sub_eqa in temp[1:]:
+    result -= sum(map(int, sub_eqa.split("+")))
 
 print(result)
 
